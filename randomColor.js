a6c9a7ef869859452617be6f43d1e4acdafca8a5
KangHeeWon <kkhw1202@gmail.com>
2021-10-31 02:09:24 +0900

    Implement fav random bg color

---
+++ randomColor.js
@@ -0,0 +1,10 @@
+const fav = document.querySelectorAll('#fav ul li');
+
+let randomColor = () => {
+    for(let i=0; i<fav.length; i++) {
+        let bgColor = `rgba( ${new Array(3).fill().map(v => Math.round(Math.random() * 255)).join(", ")},.7 )`;
+        fav[i].style.backgroundColor = bgColor;
+    }
+}
+
+randomColor();
