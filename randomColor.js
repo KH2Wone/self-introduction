b2fb7dc47bc3089dfcfd8854cf75b392de1dbe5e
KangHeeWon <kkhw1202@gmail.com>
2021-11-06 20:44:28 +0900

    Simplify IIFE of randomColor js

--- randomColor.js
+++ randomColor.js
@@ -1,10 +1,8 @@
 const fav = document.querySelectorAll('#fav ul li');
 
-let randomColor = () => {
+(() => {
     for(let i=0; i<fav.length; i++) {
         let bgColor = `rgba( ${new Array(3).fill().map(v => Math.round(Math.random() * 255)).join(", ")},.7 )`;
         fav[i].style.backgroundColor = bgColor;
     }
-}
-
-randomColor();
+})();
